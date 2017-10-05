d10bf039e247602445c1488d108c7f4f319010a9
Simbidion <simonadwyer@yahoo.com.au>
2017-10-05 15:23:32 +1100

    validations and error styling

--- app/controllers/number_converter_controller.rb
+++ app/controllers/number_converter_controller.rb
@@ -1,12 +1,21 @@
 class NumberConverterController < ApplicationController
 
   def index
-    safe_params = params.permit(:number, :base_from, :base_to)
-    if !safe_params.empty? 
-      @number = safe_params[:number].to_i
-      @base_from = safe_params[:base_from].to_i
-      @base_to = safe_params[:base_to].to_i
-    end
+    safe_params = params.permit(:number, :base)
+    @number_conversion = NumberConversion.new(safe_params)
+    @number_conversion.save if request.post?
+
+    # if !safe_params.empty? 
+    #   number = safe_params[:number].to_i
+    #   base_from = safe_params[:base_from]
+    #   base_to = safe_params[:base_to]
+    #   @number_conversion = NumberConversion.new(number: number, base: base_to)
+    #   @number_conversion.save
+    # else
+
+    # end
+
+    @past_conversions = NumberConversion.all.order(created_at: :desc)
   end
 
 end
